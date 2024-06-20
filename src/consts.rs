d85abfeea56ae9d7b137e6d6f0c3be04fbe790d7
Pierrick Curt <pierrickcurt@gmail.com>
2024-06-20 20:28:08 +0200

    chore: improve main visibility

--- src/consts.rs
+++ src/consts.rs
@@ -3,3 +3,9 @@ pub const NVS_BASE_ADDRESS: u32 = 0x9000;
 pub const NVS_DEV_EUI_ADDRESS: u32 = NVS_BASE_ADDRESS; // SIZE OF 8
 pub const NVS_APP_EUI_ADDRESS: u32 = NVS_DEV_EUI_ADDRESS + 8; // SIZE OF 8
 pub const NVS_APP_KEY_ADDRESS: u32 = NVS_APP_EUI_ADDRESS + 8; // SIZE OF 16
+
+/// Lora Max TX power
+pub const MAX_TX_POWER: u8 = 14;
+
+/// Lora data fame size in bytes
+pub const LORA_FRAME_SIZE_BYTES: usize = 24;
