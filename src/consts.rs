07b361523c1a1887a2a512ac597a7a8d67137b2c
Pierrick Curt <pierrickcurt@gmail.com>
2024-06-24 14:50:37 +0200

    chore: improve comments

--- src/consts.rs
+++ src/consts.rs
@@ -9,4 +9,28 @@ pub const NVS_WAKEUP_PERIOD_ADDRESS: u32 = NVS_APP_KEY_ADDRESS + 16; // SIZE OF 
 pub const MAX_TX_POWER: u8 = 14;
 
 /// Lora data fame size in bytes
+/// Fields (bytes)
+/// - [0] device id
+/// - [1] Vbat (value in mv - 3000 / 5)
+/// - [2] HX711 value bit 0 to 7
+/// - [3] HX711 value bit 8 to 15
+/// - [4] HX711 value bit 16 to 23
+/// - [5] BSSID number 1
+/// - [6] BSSID number 1
+/// - [7] BSSID number 1
+/// - [8] BSSID number 1
+/// - [9] BSSID number 1
+/// - [10] BSSID number 1
+/// - [11] BSSID number 2
+/// - [12] BSSID number 2
+/// - [13] BSSID number 2
+/// - [14] BSSID number 2
+/// - [15] BSSID number 2
+/// - [16] BSSID number 2
+/// - [17] BSSID number 3
+/// - [18] BSSID number 3
+/// - [19] BSSID number 3
+/// - [20] BSSID number 3
+/// - [21] BSSID number 3
+/// - [22] BSSID number 3
 pub const LORA_FRAME_SIZE_BYTES: usize = 24;
