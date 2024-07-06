7a1560009aec631a76926941393acd1041a4348a
Pierrick Curt <pierrickcurt@gmail.com>
2024-07-06 15:06:24 +0200

    chore: create function in cli module do improve lisibility

--- src/cli.rs
+++ src/cli.rs
@@ -4,12 +4,17 @@ use crate::consts::{
     NVS_APP_EUI_ADDRESS, NVS_APP_KEY_ADDRESS, NVS_DEV_EUI_ADDRESS, NVS_WAKEUP_PERIOD_ADDRESS,
 };
 use embassy_time::{Duration, Timer};
-use embedded_cli::cli::CliBuilder;
+use embedded_cli::cli::{CliBuilder, CliHandle};
 use embedded_cli::Command;
 use embedded_storage::{ReadStorage, Storage};
 use esp_hal::{peripherals::USB_DEVICE, reset::software_reset, usb_serial_jtag::UsbSerialJtag};
 use esp_storage::FlashStorage;
 use ufmt::uwrite;
+
+const EUI_SIZE_BYTES: usize = 8;
+const EUI_CHAR_NB: usize = 2 * EUI_SIZE_BYTES; // Each byte is sent as two characters
+const OTAA_SIZE_BYTES: usize = 16;
+const OTAA_CHAR_NB: usize = 2 * OTAA_SIZE_BYTES; // Each byte is sent as two characters
 
 #[derive(Command)]
 enum Base<'a> {
@@ -96,24 +101,116 @@ impl embedded_io::Write for Writer {
     }
 }
 
-fn eui_to_hex_bytes(eui: &str) -> [u8; 8] {
-    let mut bytes = [0u8; 8];
+fn eui_to_hex_bytes(eui: &str) -> [u8; EUI_SIZE_BYTES] {
+    let mut bytes = [0u8; EUI_SIZE_BYTES];
 
-    for i in 0..8 {
+    for i in 0..EUI_SIZE_BYTES {
         let byte_str = &eui[2 * i..2 * i + 2];
         bytes[i] = u8::from_str_radix(byte_str, 16).unwrap();
     }
     bytes
 }
 
-fn otaa_to_hex_bytes(otaa: &str) -> [u8; 16] {
-    let mut bytes = [0u8; 16];
+fn otaa_to_hex_bytes(otaa: &str) -> [u8; OTAA_SIZE_BYTES] {
+    let mut bytes = [0u8; OTAA_SIZE_BYTES];
 
-    for i in 0..16 {
+    for i in 0..OTAA_SIZE_BYTES {
         let byte_str = &otaa[2 * i..2 * i + 2];
         bytes[i] = u8::from_str_radix(byte_str, 16).unwrap();
     }
     bytes
+}
+
+fn handle_eui_command(
+    cli: &mut CliHandle<'_, Writer, Infallible>,
+    flash: &mut FlashStorage,
+    address: u32,
+    command: EuiCommand,
+) -> Result<(), Infallible> {
+    let mut eui = [0u8; EUI_SIZE_BYTES];
+    match command {
+        EuiCommand::Get => {
+            flash.read(address, &mut eui).unwrap();
+            uwrite!(cli.writer(), "Current EUI is {:?}", &eui[..EUI_SIZE_BYTES])?;
+        }
+        EuiCommand::Set { value } => {
+            if value.len() != EUI_CHAR_NB {
+                uwrite!(
+                    cli.writer(),
+                    "Invalid size {:?}, expected {:?} ",
+                    value.len(),
+                    EUI_CHAR_NB
+                )?;
+            } else {
+                flash
+                    .write(address, &eui_to_hex_bytes(value)[..EUI_SIZE_BYTES])
+                    .unwrap();
+                uwrite!(cli.writer(), "Set EUI is {:?}", eui_to_hex_bytes(value))?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn handle_app_key_command(
+    cli: &mut CliHandle<'_, Writer, Infallible>,
+    flash: &mut FlashStorage,
+    address: u32,
+    command: AppKeyCommand,
+) -> Result<(), Infallible> {
+    let mut app_key = [0u8; OTAA_SIZE_BYTES];
+    match command {
+        AppKeyCommand::Get => {
+            flash.read(address, &mut app_key).unwrap();
+            uwrite!(cli.writer(), "Current AppKey is {:?}", &app_key[..16])?;
+        }
+        AppKeyCommand::Set { value } => {
+            if value.len() != OTAA_CHAR_NB {
+                uwrite!(
+                    cli.writer(),
+                    "Invalid size {:?}, expected {:?} ",
+                    value.len(),
+                    OTAA_CHAR_NB
+                )?;
+            } else {
+                flash
+                    .write(address, &otaa_to_hex_bytes(value)[..OTAA_SIZE_BYTES])
+                    .unwrap();
+                uwrite!(cli.writer(), "Set AppKey is {:?}", otaa_to_hex_bytes(value))?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn handle_wakeup_command(
+    cli: &mut CliHandle<'_, Writer, Infallible>,
+    flash: &mut FlashStorage,
+    address: u32,
+    command: WakeUpCommand,
+) -> Result<(), Infallible> {
+    let mut raw_value = [0u8; 2];
+    match command {
+        WakeUpCommand::Get => {
+            flash.read(address, &mut raw_value).unwrap();
+            uwrite!(
+                cli.writer(),
+                "Current WakeUp is {:?}",
+                u16::from_be_bytes(raw_value)
+            )?;
+        }
+        WakeUpCommand::Set { value } => {
+            flash.write(address, &value.to_be_bytes()).unwrap();
+            uwrite!(cli.writer(), "Set WakeUp is {:?}", value)?;
+        }
+    }
+    Ok(())
+}
+
+fn handle_reset_command(cli: &mut CliHandle<'_, Writer, Infallible>) -> Result<(), Infallible> {
+    uwrite!(cli.writer(), "Reset now !")?;
+    software_reset();
+    Ok(())
 }
 
 #[embassy_executor::task]
@@ -148,99 +245,29 @@ pub async fn cli_run(usb_periph: USB_DEVICE) {
             let _ = cli.process_byte::<Base, _>(
                 value,
                 &mut Base::processor(|cli, command| {
-                    match command {
+                    let _ = match command {
                         Base::DevEui { command } => {
-                            let mut dev_eui = [0u8; 8];
-                            match command {
-                                EuiCommand::Get => {
-                                    flash.read(NVS_DEV_EUI_ADDRESS, &mut dev_eui).unwrap();
-                                    uwrite!(cli.writer(), "Current DevEui is {:?}", &dev_eui[..8])?;
-                                }
-                                EuiCommand::Set { value } => {
-                                    flash
-                                        .write(NVS_DEV_EUI_ADDRESS, &eui_to_hex_bytes(value)[..8])
-                                        .unwrap();
-                                    uwrite!(
-                                        cli.writer(),
-                                        "Set DevEui is {:?}",
-                                        eui_to_hex_bytes(value)
-                                    )?;
-                                }
-                            }
+                            handle_eui_command(cli, &mut flash, NVS_DEV_EUI_ADDRESS, command)
                         }
                         Base::AppEui { command } => {
-                            let mut app_eui = [0u8; 8];
-                            match command {
-                                EuiCommand::Get => {
-                                    flash.read(NVS_APP_EUI_ADDRESS, &mut app_eui).unwrap();
-                                    uwrite!(cli.writer(), "Current AppEui is {:?}", &app_eui[..8])?;
-                                }
-                                EuiCommand::Set { value } => {
-                                    flash
-                                        .write(NVS_APP_EUI_ADDRESS, &eui_to_hex_bytes(value)[..8])
-                                        .unwrap();
-                                    uwrite!(
-                                        cli.writer(),
-                                        "Set AppEui is {:?}",
-                                        eui_to_hex_bytes(value)
-                                    )?;
-                                }
-                            }
+                            handle_eui_command(cli, &mut flash, NVS_APP_EUI_ADDRESS, command)
                         }
                         Base::AppKey { command } => {
-                            let mut app_key = [0u8; 16];
-                            match command {
-                                AppKeyCommand::Get => {
-                                    flash.read(NVS_APP_KEY_ADDRESS, &mut app_key).unwrap();
-                                    uwrite!(
-                                        cli.writer(),
-                                        "Current AppKey is {:?}",
-                                        &app_key[..16]
-                                    )?;
-                                }
-                                AppKeyCommand::Set { value } => {
-                                    flash
-                                        .write(NVS_APP_KEY_ADDRESS, &otaa_to_hex_bytes(value)[..16])
-                                        .unwrap();
-                                    uwrite!(
-                                        cli.writer(),
-                                        "Set AppKey is {:?}",
-                                        otaa_to_hex_bytes(value)
-                                    )?;
-                                }
-                            }
+                            handle_app_key_command(cli, &mut flash, NVS_APP_KEY_ADDRESS, command)
                         }
-                        Base::WakeUp { command } => {
-                            let mut raw_value = [0u8; 2];
-                            match command {
-                                WakeUpCommand::Get => {
-                                    flash
-                                        .read(NVS_WAKEUP_PERIOD_ADDRESS, &mut raw_value)
-                                        .unwrap();
-                                    uwrite!(
-                                        cli.writer(),
-                                        "Current WakeUp is {:?}",
-                                        u16::from_be_bytes(raw_value)
-                                    )?;
-                                }
-                                WakeUpCommand::Set { value } => {
-                                    flash
-                                        .write(NVS_WAKEUP_PERIOD_ADDRESS, &value.to_be_bytes())
-                                        .unwrap();
-                                    uwrite!(cli.writer(), "Set WakeUp is {:?}", value)?;
-                                }
-                            }
-                        }
-                        Base::Reset => {
-                            uwrite!(cli.writer(), "Reset now !")?;
-                            software_reset();
-                        }
-                    }
+                        Base::WakeUp { command } => handle_wakeup_command(
+                            cli,
+                            &mut flash,
+                            NVS_WAKEUP_PERIOD_ADDRESS,
+                            command,
+                        ),
+                        Base::Reset => handle_reset_command(cli),
+                    };
                     Ok(())
                 }),
             );
         }
-        // Wait 10ms between each byte read, bytes are buffered by peripheral
-        Timer::after(Duration::from_millis(10)).await;
+        // Wait 1ms between each byte read, bytes are buffered by peripheral
+        Timer::after(Duration::from_millis(1)).await;
     }
 }
